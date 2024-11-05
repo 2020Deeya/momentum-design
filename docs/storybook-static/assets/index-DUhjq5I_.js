dc3aa8fadd5db1c9d2e748376729be1a1aed90e6
Momentum Design Automation <automation@momentum.design>
2024-11-05 11:48:44 +0000

    ci(docs): deploy documentation [skip ci] [skip release]

---
+++ docs/storybook-static/assets/index-DUhjq5I_.js
@@ -0,0 +1,240 @@
+import{i as g,k as a}from"./lit-element-CPYlYYac.js";import{u,C as p,n as h}from"./index-BLShQaRP.js";const x=u.constructTagName("text"),b={BODY_SMALL_REGULAR:"body-small-regular",BODY_SMALL_MEDIUM:"body-small-medium",BODY_SMALL_BOLD:"body-small-bold",BODY_MIDSIZE_REGULAR:"body-midsize-regular",BODY_MIDSIZE_MEDIUM:"body-midsize-medium",BODY_MIDSIZE_BOLD:"body-midsize-bold",BODY_LARGE_REGULAR:"body-large-regular",BODY_LARGE_MEDIUM:"body-large-medium",BODY_LARGE_BOLD:"body-large-bold",BODY_SMALL_REGULAR_UNDERLINE:"body-small-regular-underline",BODY_SMALL_MEDIUM_UNDERLINE:"body-small-medium-underline",BODY_MIDSIZE_REGULAR_UNDERLINE:"body-midsize-regular-underline",BODY_MIDSIZE_MEDIUM_UNDERLINE:"body-midsize-medium-underline",BODY_LARGE_REGULAR_UNDERLINE:"body-large-regular-underline",BODY_LARGE_MEDIUM_UNDERLINE:"body-large-medium-underline",HEADING_SMALL_REGULAR:"heading-small-regular",HEADING_SMALL_MEDIUM:"heading-small-medium",HEADING_SMALL_BOLD:"heading-small-bold",HEADING_MIDSIZE_REGULAR:"heading-midsize-regular",HEADING_MIDSIZE_MEDIUM:"heading-midsize-medium",HEADING_MIDSIZE_BOLD:"heading-midsize-bold",HEADING_LARGE_REGULAR:"heading-large-regular",HEADING_LARGE_MEDIUM:"heading-large-medium",HEADING_LARGE_BOLD:"heading-large-bold",HEADING_XLARGE_REGULAR:"heading-xlarge-regular",HEADING_XLARGE_MEDIUM:"heading-xlarge-medium",HEADING_XLARGE_BOLD:"heading-xlarge-bold",HEADLINE_SMALL_LIGHT:"headline-small-light",HEADLINE_SMALL_REGULAR:"headline-small-regular"},t={H1:"h1",H2:"h2",H3:"h3",H4:"h4",H5:"h5",H6:"h6",P:"p",SMALL:"small",SPAN:"span",DIV:"div"},e={TYPE:b.BODY_LARGE_REGULAR,TEXT_ELEMENT_TAGNAME:t.P,CSS_PART_TEXT:"text",CHILDREN:"The quick brown fox jumps over the lazy dog"},v=g`
+  :host([type="headline-small-regular"])::part(text) {
+    font-size: var(--mds-font-apps-headline-small-regular-font-size);
+    font-weight: var(--mds-font-apps-headline-small-regular-font-weight);
+    line-height: var(--mds-font-apps-headline-small-regular-line-height);
+    text-decoration: var(--mds-font-apps-headline-small-regular-text-decoration);
+    text-transform: var(--mds-font-apps-headline-small-regular-text-case);
+  }
+
+  :host([type="headline-small-light"])::part(text) {
+    font-size: var(--mds-font-apps-headline-small-light-font-size);
+    font-weight: var(--mds-font-apps-headline-small-light-font-weight);
+    line-height: var(--mds-font-apps-headline-small-light-line-height);
+    text-decoration: var(--mds-font-apps-headline-small-light-text-decoration);
+    text-transform: var(--mds-font-apps-headline-small-light-text-case);
+  }
+
+  :host([type="heading-xlarge-bold"])::part(text) {
+    font-size: var(--mds-font-apps-heading-xlarge-bold-font-size);
+    font-weight: var(--mds-font-apps-heading-xlarge-bold-font-weight);
+    line-height: var(--mds-font-apps-heading-xlarge-bold-line-height);
+    text-decoration: var(--mds-font-apps-heading-xlarge-bold-text-decoration);
+    text-transform: var(--mds-font-apps-heading-xlarge-bold-text-case);
+  }
+  
+  :host([type="heading-xlarge-medium"])::part(text) {
+    font-size: var(--mds-font-apps-heading-xlarge-medium-font-size);
+    font-weight: var(--mds-font-apps-heading-xlarge-medium-font-weight);
+    line-height: var(--mds-font-apps-heading-xlarge-medium-line-height);
+    text-decoration: var(--mds-font-apps-heading-xlarge-medium-text-decoration);
+    text-transform: var(--mds-font-apps-heading-xlarge-medium-text-case);
+  }
+  
+  :host([type="heading-xlarge-regular"])::part(text) {
+    font-size: var(--mds-font-apps-heading-xlarge-regular-font-size);
+    font-weight: var(--mds-font-apps-heading-xlarge-regular-font-weight);
+    line-height: var(--mds-font-apps-heading-xlarge-regular-line-height);
+    text-decoration: var(--mds-font-apps-heading-xlarge-regular-text-decoration);
+    text-transform: var(--mds-font-apps-heading-xlarge-regular-text-case);
+  }
+
+  :host([type="heading-large-bold"])::part(text) {
+    font-size: var(--mds-font-apps-heading-large-bold-font-size);
+    font-weight: var(--mds-font-apps-heading-large-bold-font-weight);
+    line-height: var(--mds-font-apps-heading-large-bold-line-height);
+    text-decoration: var(--mds-font-apps-heading-large-bold-text-decoration);
+    text-transform: var(--mds-font-apps-heading-large-bold-text-case);
+  }
+  
+  :host([type="heading-large-medium"])::part(text) {
+    font-size: var(--mds-font-apps-heading-large-medium-font-size);
+    font-weight: var(--mds-font-apps-heading-large-medium-font-weight);
+    line-height: var(--mds-font-apps-heading-large-medium-line-height);
+    text-decoration: var(--mds-font-apps-heading-large-medium-text-decoration);
+    text-transform: var(--mds-font-apps-heading-large-medium-text-case);
+  }
+  
+  :host([type="heading-large-regular"])::part(text) {
+    font-size: var(--mds-font-apps-heading-large-regular-font-size);
+    font-weight: var(--mds-font-apps-heading-large-regular-font-weight);
+    line-height: var(--mds-font-apps-heading-large-regular-line-height);
+    text-decoration: var(--mds-font-apps-heading-large-regular-text-decoration);
+    text-transform: var(--mds-font-apps-heading-large-regular-text-case);
+  }
+
+  :host([type="heading-midsize-bold"])::part(text) {
+    font-size: var(--mds-font-apps-heading-midsize-bold-font-size);
+    font-weight: var(--mds-font-apps-heading-midsize-bold-font-weight);
+    line-height: var(--mds-font-apps-heading-midsize-bold-line-height);
+    text-decoration: var(--mds-font-apps-heading-midsize-bold-text-decoration);
+    text-transform: var(--mds-font-apps-heading-midsize-bold-text-case);
+  }
+  
+  :host([type="heading-midsize-medium"])::part(text) {
+    font-size: var(--mds-font-apps-heading-midsize-medium-font-size);
+    font-weight: var(--mds-font-apps-heading-midsize-medium-font-weight);
+    line-height: var(--mds-font-apps-heading-midsize-medium-line-height);
+    text-decoration: var(--mds-font-apps-heading-midsize-medium-text-decoration);
+    text-transform: var(--mds-font-apps-heading-midsize-medium-text-case);
+  }
+  
+  :host([type="heading-midsize-regular"])::part(text) {
+    font-size: var(--mds-font-apps-heading-midsize-regular-font-size);
+    font-weight: var(--mds-font-apps-heading-midsize-regular-font-weight);
+    line-height: var(--mds-font-apps-heading-midsize-regular-line-height);
+    text-decoration: var(--mds-font-apps-heading-midsize-regular-text-decoration);
+    text-transform: var(--mds-font-apps-heading-midsize-regular-text-case);
+  }
+
+  :host([type="heading-small-bold"])::part(text) {
+    font-size: var(--mds-font-apps-heading-small-bold-font-size);
+    font-weight: var(--mds-font-apps-heading-small-bold-font-weight);
+    line-height: var(--mds-font-apps-heading-small-bold-line-height);
+    text-decoration: var(--mds-font-apps-heading-small-bold-text-decoration);
+    text-transform: var(--mds-font-apps-heading-small-bold-text-case);
+  }
+  
+  :host([type="heading-small-medium"])::part(text) {
+    font-size: var(--mds-font-apps-heading-small-medium-font-size);
+    font-weight: var(--mds-font-apps-heading-small-medium-font-weight);
+    line-height: var(--mds-font-apps-heading-small-medium-line-height);
+    text-decoration: var(--mds-font-apps-heading-small-medium-text-decoration);
+    text-transform: var(--mds-font-apps-heading-small-medium-text-case);
+  }
+  
+  :host([type="heading-small-regular"])::part(text) {
+    font-size: var(--mds-font-apps-heading-small-regular-font-size);
+    font-weight: var(--mds-font-apps-heading-small-regular-font-weight);
+    line-height: var(--mds-font-apps-heading-small-regular-line-height);
+    text-decoration: var(--mds-font-apps-heading-small-regular-text-decoration);
+    text-transform: var(--mds-font-apps-heading-small-regular-text-case);
+  }
+
+  :host([type="body-large-bold"])::part(text) {
+    font-size: var(--mds-font-apps-body-large-bold-font-size);
+    font-weight: var(--mds-font-apps-body-large-bold-font-weight);
+    line-height: var(--mds-font-apps-body-large-bold-line-height);
+    text-decoration: var(--mds-font-apps-body-large-bold-text-decoration);
+    text-transform: var(--mds-font-apps-body-large-bold-text-case);
+  }
+  
+  :host([type="body-large-medium-underline"])::part(text) {
+    font-size: var(--mds-font-apps-body-large-medium-underline-font-size);
+    font-weight: var(--mds-font-apps-body-large-medium-underline-font-weight);
+    line-height: var(--mds-font-apps-body-large-medium-underline-line-height);
+    text-decoration: var(--mds-font-apps-body-large-medium-underline-text-decoration);
+    text-transform: var(--mds-font-apps-body-large-medium-underline-text-case);
+  }
+  
+  :host([type="body-large-medium"])::part(text) {
+    font-size: var(--mds-font-apps-body-large-medium-font-size);
+    font-weight: var(--mds-font-apps-body-large-medium-font-weight);
+    line-height: var(--mds-font-apps-body-large-medium-line-height);
+    text-decoration: var(--mds-font-apps-body-large-medium-text-decoration);
+    text-transform: var(--mds-font-apps-body-large-medium-text-case);
+  }
+  
+  :host([type="body-large-regular-underline"])::part(text) {
+    font-size: var(--mds-font-apps-body-large-regular-underline-font-size);
+    font-weight: var(--mds-font-apps-body-large-regular-underline-font-weight);
+    line-height: var(--mds-font-apps-body-large-regular-underline-line-height);
+    text-decoration: var(--mds-font-apps-body-large-regular-underline-text-decoration);
+    text-transform: var(--mds-font-apps-body-large-regular-underline-text-case);
+  }
+  
+  :host([type="body-large-regular"])::part(text) {
+    font-size: var(--mds-font-apps-body-large-regular-font-size);
+    font-weight: var(--mds-font-apps-body-large-regular-font-weight);
+    line-height: var(--mds-font-apps-body-large-regular-line-height);
+    text-decoration: var(--mds-font-apps-body-large-regular-text-decoration);
+    text-transform: var(--mds-font-apps-body-large-regular-text-case);
+  }
+
+  :host([type="body-midsize-bold"])::part(text) {
+    font-size: var(--mds-font-apps-body-midsize-bold-font-size);
+    font-weight: var(--mds-font-apps-body-midsize-bold-font-weight);
+    line-height: var(--mds-font-apps-body-midsize-bold-line-height);
+    text-decoration: var(--mds-font-apps-body-midsize-bold-text-decoration);
+    text-transform: var(--mds-font-apps-body-midsize-bold-text-case);
+  }
+  
+  :host([type="body-midsize-medium-underline"])::part(text) {
+    font-size: var(--mds-font-apps-body-midsize-medium-underline-font-size);
+    font-weight: var(--mds-font-apps-body-midsize-medium-underline-font-weight);
+    line-height: var(--mds-font-apps-body-midsize-medium-underline-line-height);
+    text-decoration: var(--mds-font-apps-body-midsize-medium-underline-text-decoration);
+    text-transform: var(--mds-font-apps-body-midsize-medium-underline-text-case);
+  }
+  
+  :host([type="body-midsize-medium"])::part(text) {
+    font-size: var(--mds-font-apps-body-midsize-medium-font-size);
+    font-weight: var(--mds-font-apps-body-midsize-medium-font-weight);
+    line-height: var(--mds-font-apps-body-midsize-medium-line-height);
+    text-decoration: var(--mds-font-apps-body-midsize-medium-text-decoration);
+    text-transform: var(--mds-font-apps-body-midsize-medium-text-case);
+  }
+  
+  :host([type="body-midsize-regular-underline"])::part(text) {
+    font-size: var(--mds-font-apps-body-midsize-regular-underline-font-size);
+    font-weight: var(--mds-font-apps-body-midsize-regular-underline-font-weight);
+    line-height: var(--mds-font-apps-body-midsize-regular-underline-line-height);
+    text-decoration: var(--mds-font-apps-body-midsize-regular-underline-text-decoration);
+    text-transform: var(--mds-font-apps-body-midsize-regular-underline-text-case);
+  }
+  
+  :host([type="body-midsize-regular"])::part(text) {
+    font-size: var(--mds-font-apps-body-midsize-regular-font-size);
+    font-weight: var(--mds-font-apps-body-midsize-regular-font-weight);
+    line-height: var(--mds-font-apps-body-midsize-regular-line-height);
+    text-decoration: var(--mds-font-apps-body-midsize-regular-text-decoration);
+    text-transform: var(--mds-font-apps-body-midsize-regular-text-case);
+  }
+
+  :host([type="body-small-bold"])::part(text) {
+    font-size: var(--mds-font-apps-body-small-bold-font-size);
+    font-weight: var(--mds-font-apps-body-small-bold-font-weight);
+    line-height: var(--mds-font-apps-body-small-bold-line-height);
+    text-decoration: var(--mds-font-apps-body-small-bold-text-decoration);
+    text-transform: var(--mds-font-apps-body-small-bold-text-case);
+  }
+  
+  :host([type="body-small-medium-underline"])::part(text) {
+    font-size: var(--mds-font-apps-body-small-medium-underline-font-size);
+    font-weight: var(--mds-font-apps-body-small-medium-underline-font-weight);
+    line-height: var(--mds-font-apps-body-small-medium-underline-line-height);
+    text-decoration: var(--mds-font-apps-body-small-medium-underline-text-decoration);
+    text-transform: var(--mds-font-apps-body-small-medium-underline-text-case);
+  }
+  
+  :host([type="body-small-medium"])::part(text) {
+    font-size: var(--mds-font-apps-body-small-medium-font-size);
+    font-weight: var(--mds-font-apps-body-small-medium-font-weight);
+    line-height: var(--mds-font-apps-body-small-medium-line-height);
+    text-decoration: var(--mds-font-apps-body-small-medium-text-decoration);
+    text-transform: var(--mds-font-apps-body-small-medium-text-case);
+  }
+  
+  :host([type="body-small-regular-underline"])::part(text) {
+    font-size: var(--mds-font-apps-body-small-regular-underline-font-size);
+    font-weight: var(--mds-font-apps-body-small-regular-underline-font-weight);
+    line-height: var(--mds-font-apps-body-small-regular-underline-line-height);
+    text-decoration: var(--mds-font-apps-body-small-regular-underline-text-decoration);
+    text-transform: var(--mds-font-apps-body-small-regular-underline-text-case);
+  }
+  
+  :host([type="body-small-regular"])::part(text) {
+    font-size: var(--mds-font-apps-body-small-regular-font-size);
+    font-weight: var(--mds-font-apps-body-small-regular-font-weight);
+    line-height: var(--mds-font-apps-body-small-regular-line-height);
+    text-decoration: var(--mds-font-apps-body-small-regular-text-decoration);
+    text-transform: var(--mds-font-apps-body-small-regular-text-case);
+  }
+`,y=[g`
+    :host {
+      --mdc-text-font-family: var(--mdc-themeprovider-font-family);
+
+      display: block;
+      font-family: var(--mdc-text-font-family);
+    }
+  `,v];var c=Object.defineProperty,f=(n,r,l,z)=>{for(var i=void 0,d=n.length-1,m;d>=0;d--)(m=n[d])&&(i=m(r,l,i)||i);return i&&c(r,l,i),i};const s=class s extends p{constructor(){super(...arguments),this.type=e.TYPE,this.tagname=e.TEXT_ELEMENT_TAGNAME}render(){switch(this.tagname){case t.H1:return a`<h1 part=${e.CSS_PART_TEXT}><slot></slot></h1>`;case t.H2:return a`<h2 part=${e.CSS_PART_TEXT}><slot></slot></h2>`;case t.H3:return a`<h3 part=${e.CSS_PART_TEXT}><slot></slot></h3>`;case t.H4:return a`<h4 part=${e.CSS_PART_TEXT}><slot></slot></h4>`;case t.H5:return a`<h5 part=${e.CSS_PART_TEXT}><slot></slot></h5>`;case t.H6:return a`<h6 part=${e.CSS_PART_TEXT}><slot></slot></h6>`;case t.DIV:return a`<div part=${e.CSS_PART_TEXT}><slot></slot></div>`;case t.SPAN:return a`<span part=${e.CSS_PART_TEXT}><slot></slot></span>`;case t.SMALL:return a`<small part=${e.CSS_PART_TEXT}><slot></slot></small>`;case t.P:default:return a`<p part=${e.CSS_PART_TEXT}><slot></slot></p>`}}};s.styles=[...p.styles,...y];let o=s;f([h({attribute:"type",reflect:!0,type:String})],o.prototype,"type");f([h({attribute:"tagname",reflect:!0,type:String})],o.prototype,"tagname");o.register(x);export{e as D,b as F,t as V};
